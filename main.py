eeeb1bcae0adc1e7e7d5cef71e6c072660cd3185
yusufkt0 <yusufkayrataner3@gmail.com>
2024-07-24 23:55:29 +0300

    rewriting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-file="passwd.md"
+file="passwds.md"
 import random
 import time
 import string
@@ -9,11 +9,11 @@ if input("") == "n":
     pwd_length = int(input("password length:"))
     p = []
     for x in range(0,l):
-        p.insert(l,random.choice(string.ascii_letters + string.digits + string.punctuation))
-        print(p,"\n")
+        p.insert(pwd_length, random.choice(string.ascii_letters + string.digits + string.punctuation))
+        print(p, "\n")
         print("Do you want to save the password to a file? (Y/N, [custom/file/path])")
         if input("Your Choice:") == "Y" or "y":
-            print("")#####
+            print("")####
             
 
         
